3896b2f899fb7de62744e36c1e1a251329f0323e
zayennn <zaayeennn@gmail.com>
2026-04-22 10:35:19 +0700

    update

--- latihan/src/app/Main.java
+++ latihan/src/app/Main.java
@@ -1,11 +1,15 @@
 package app;
 
+import modules.Mahasiswa;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        ArrayList<Mahasiswa> mahasiswas = new ArrayList<>();
 
+        mahasiswas.
     }
 }
