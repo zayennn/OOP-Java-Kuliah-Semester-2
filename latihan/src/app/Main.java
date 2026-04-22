965351a1f336c9ddd940ed76b27bfd1de73f581b
zayennn <zaayeennn@gmail.com>
2026-04-22 10:32:57 +0700

    update

--- latihan/src/app/Main.java
+++ latihan/src/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
     }
 }
