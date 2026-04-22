d6597728e2691bbe46b278423038c6b14626de38
zayennn <zaayeennn@gmail.com>
2026-04-22 13:17:13 +0700

    update

--- latihan/src/app/Main.java
+++ latihan/src/app/Main.java
@@ -10,5 +10,9 @@ public class Main {
         Scanner input = new Scanner(System.in);
         ArrayList<Mahasiswa> mahasiswas = new ArrayList<>();
 
+        mahasiswas.addAll(
+                new Mahasiswa("elang", 15250506, 85),
+                new Mahasiswa("capyking", 15250708, 90)
+        );
     }
 }
