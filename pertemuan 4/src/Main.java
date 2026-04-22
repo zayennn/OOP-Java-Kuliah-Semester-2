ef7d97cda369324c3a0bd122f69d289697a40d55
zayennn <zaayeennn@gmail.com>
2026-04-22 10:30:08 +0700

    update

--- pertemuan 4/src/Main.java
+++ pertemuan 4/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        for ( int i = 0; i <= 5; i++ ) {
+            for ( int j = i; j < i; j++ ) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
     }
 }
