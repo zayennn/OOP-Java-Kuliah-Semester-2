2ff6b734a86fe0adf6768bdca9fce5493163a304
zayennn <zaayeennn@gmail.com>
2026-04-22 10:36:49 +0700

    update

--- latihan/src/app/Main.java
+++ latihan/src/app/Main.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<Mahasiswa> mahasiswas = new ArrayList<>();
+        Mahasiswa mahasiswas = new Mahasiswa();
 
-        mahasiswas.
+
     }
 }
