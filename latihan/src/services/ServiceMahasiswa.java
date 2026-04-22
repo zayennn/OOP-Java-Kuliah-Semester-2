ff9bbd3164a320f57379d91a9d6235a711d8203d
zayennn <zaayeennn@gmail.com>
2026-04-22 13:24:06 +0700

    update

--- latihan/src/services/ServiceMahasiswa.java
+++ latihan/src/services/ServiceMahasiswa.java
@@ -1,4 +1,13 @@
 package services;
 
+import modules.Mahasiswa;
+
 public class ServiceMahasiswa {
+    public void getInfoMahasiswas(Mahasiswa mhs) {
+        System.out.printf("""
+                Nama mahasiswa  : %s
+                NIM mahasiswa   : %d
+                Nilai mahasiswa : %d
+                """, mhs.nama, mhs.nim, mhs.nilai);
+    }
 }
