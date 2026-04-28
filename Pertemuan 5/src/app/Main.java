4ddd8febc5ac6b2819a017f241018a0ccf6afe46
zayennn <zaayeennn@gmail.com>
2026-04-28 12:14:32 +0700

    update

--- Pertemuan 5/src/app/Main.java
+++ Pertemuan 5/src/app/Main.java
@@ -4,14 +4,12 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\athaz\\IdeaProjects\\Kuliah Semester 2\\src\\app\\data.txt");
+        File file = new File("src/app/data.txt");
 
         if (file.exists()) {
             System.out.println("file aja");
         } else {
             System.out.println("file tidak ada");
         }
-
-//        System.out.println(file.getAbsolutePath());
     }
 }
