10220f9a658defbeb47fb657feec9a2ada6e28f9
zayennn <zaayeennn@gmail.com>
2026-04-28 12:10:24 +0700

    update

--- Pertemuan 5/src/app/Main.java
+++ Pertemuan 5/src/app/Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("data.txt");
+        File file = new File("./data.txt");
 
         if (file.exists()) {
             System.out.println("file aja");
