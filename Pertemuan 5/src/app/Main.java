874c696655f1726c13cc1ebef471cdd133a5f0cb
zayennn <zaayeennn@gmail.com>
2026-04-28 12:47:26 +0700

    update

--- Pertemuan 5/src/app/Main.java
+++ Pertemuan 5/src/app/Main.java
@@ -11,8 +11,14 @@ public class Main {
         double[] harga, jumlah_harga, pajak, total_bayar;
 
         jenis = new String[10];
-        jenis[1] = "baju";
+        jenis[0] = "baju";
+        jenis[1] = "sepatu";
+        jenis[2] = "kaos";
+        jenis[3] = "jaket";
+        jenis[4] = "celana";
 
-        System.out.println(jenis[0]);
+        for ( int k = 0; k < jenis.length; k++ ) {
+            System.out.println(k);
+        }
     }
 }
