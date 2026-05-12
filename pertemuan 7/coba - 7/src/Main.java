b383c4429a32bae424652b9fe53e09971988415b
zayennn <zaayeennn@gmail.com>
2026-05-12 12:28:17 +0700

    update

---
+++ pertemuan 7/coba - 7/src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("flexbox: flex direction");
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(
+                new BoxLayout(panel, BoxLayout.Y_AXIS)
+        );
+
+        panel.add(new JLabel("masukan nama anda : "));
+        panel.add(new JTextField(15));
+        panel.add(new JButton("check nama kamu"));
+
+        frame.add(panel);
+    }
+}
