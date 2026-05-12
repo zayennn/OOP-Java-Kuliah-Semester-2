ed40166f230f23249e905b5f1e911183c68dfb97
zayennn <zaayeennn@gmail.com>
2026-05-12 11:25:43 +0700

    update

--- pertemuan 7/src/Main.java
+++ pertemuan 7/src/Main.java
@@ -1,8 +1,15 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("aplikasi pertama: hello world");
+        JFrame frame = new JFrame("aplikasi pertama");
+        JButton button = new JButton("click aku");
+
+        button.setBounds(100, 100, 100, 50);
+
+        frame.add(button);
 
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
