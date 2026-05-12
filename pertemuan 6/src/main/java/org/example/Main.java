5866f4778bc5b0bd71cfac0d0213b68716275dce
zayennn <zaayeennn@gmail.com>
2026-05-12 11:08:46 +0700

    update

--- pertemuan 6/src/main/java/org/example/Main.java
+++ pertemuan 6/src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static class Koneksi {
+        private static final String url = "jdbc:mysql://localhost:3306/db_kuliah_java";
+        private static final String user = "root";
+        private static final String password = "";
+
+        public static Connection getConnection() throws SQLException {
+            return DriverManager.getConnection(url, user, password);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = Koneksi.getConnection()) {
+            String sql = "insert into mahasiswa (id, name, nim, nilai) values (?, ?, ?, ?)";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, 1);
+
+        } catch (SQLException ex) {
+
         }
     }
 }
