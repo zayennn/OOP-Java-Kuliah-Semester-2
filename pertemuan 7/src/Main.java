f2c630046ce5f94ffee9eb37e2d9bebaf9216afb
zayennn <zaayeennn@gmail.com>
2026-05-12 11:37:51 +0700

    update

--- pertemuan 7/src/Main.java
+++ pertemuan 7/src/Main.java
@@ -8,7 +8,24 @@ public class Main {
         JFrame frame = new JFrame("aplikasi pertama");
         frame.setLayout(null);
 
+        JLabel label1 = new JLabel("masukan nama anda: ");
+        label1.setBounds(50, 50, 300, 50);
 
+        JButton button1 = new JButton("selesai");
+        button1.setBounds(20, 100, 100, 50);
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nama = label1.getText().trim();
+
+                if(nama.equalsIgnoreCase(nama)) {
+                    throw new IllegalArgumentException("nama tidak boleh kosong");
+                } else {
+                    JOptionPane.showMessageDialog(null, String.format("halo %s", nama));
+                }
+            }
+        });
 
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
