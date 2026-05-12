7fd1756771051c9cecefd404c836e969401a0354
zayennn <zaayeennn@gmail.com>
2026-05-12 11:16:28 +0700

    update

--- pertemuan 7/src/Main.java
+++ pertemuan 7/src/Main.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("aplikasi pertama: hello world");
 
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
